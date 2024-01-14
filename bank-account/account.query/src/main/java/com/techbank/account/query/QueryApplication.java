2f1e3531567f1b3ffd61020938ee14918c4fe421
khalil-chermiti <khalil666chermiti@gmail.com>
2024-01-14 22:43:28 +0100

    feat(account): implement account query handler

--- bank-account/account.query/src/main/java/com/techbank/account/query/QueryApplication.java
+++ bank-account/account.query/src/main/java/com/techbank/account/query/QueryApplication.java
@@ -1,13 +1,19 @@
 package com.techbank.account.query;
 
+import com.techbank.account.query.api.queries.QueryHandler;
+import com.techbank.cqrs.core.infrastructure.QueryDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QueryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QueryApplication.class, args);
-	}
+    @Autowired
+    private QueryDispatcher queryDispatcher;
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryApplication.class, args);
+    }
 
 }
--- bank-account/account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountsWithBalanceQuery.java
+++ bank-account/account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountsWithBalanceQuery.java
@@ -2,8 +2,12 @@ package com.techbank.account.query.api.queries;
 
 import com.techbank.account.query.api.dto.EqualityTypeDto;
 import com.techbank.cqrs.core.query.BaseQuery;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-public class findAccountsWithBalanceQuery extends BaseQuery {
+@Data
+@AllArgsConstructor
+public class FindAccountsWithBalanceQuery extends BaseQuery {
     private EqualityTypeDto equalityType;
     private double balance;
 }
